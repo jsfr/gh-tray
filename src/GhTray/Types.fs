8b18e4ab0810d7ff00c29895d4e91a8b6d52938d
Jens Ejbye Fredskov <jsfr@users.noreply.github.com>
2026-03-18 08:58:53 +0000

    feat: add domain types for PR status and GitHub client

---
+++ src/GhTray/Types.fs
@@ -0,0 +1,44 @@
+namespace GhTray
+
+type CheckStatus =
+    | Success
+    | Failure
+    | Pending
+
+type ReviewStatus =
+    | Approved
+    | ChangesRequested
+    | ReviewRequired
+
+type PullRequest =
+    { Title: string
+      Url: string
+      Number: int
+      Repository: string
+      IsDraft: bool
+      CheckStatus: CheckStatus option
+      ReviewStatus: ReviewStatus option
+      HasConflicts: bool }
+
+type PullRequestGroup =
+    { Mine: PullRequest list
+      ReviewRequested: PullRequest list
+      Involved: PullRequest list }
+
+module PullRequestGroup =
+    let empty =
+        { Mine = []
+          ReviewRequested = []
+          Involved = [] }
+
+    let totalCount (group: PullRequestGroup) =
+        List.length group.Mine
+        + List.length group.ReviewRequested
+        + List.length group.Involved
+
+type PollerOptions =
+    { PollInterval: System.TimeSpan }
+
+type IGitHubClient =
+    abstract member GetUsername: unit -> Async<string>
+    abstract member FetchPullRequests: username: string -> Async<PullRequestGroup>
